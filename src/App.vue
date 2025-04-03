1ce4069e019563e42a21a5b9e9467276f85c7d62
XieZongChen <952937570@qq.com>
2025-04-03 14:44:30 +0800

    feat: 初始化 three.js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import * as THREE from 'three';
+import { onMounted, ref, useTemplateRef } from 'vue';
+
+const contentRef = useTemplateRef('content');
+onMounted(() => {
+  initThree();
+  animate();
+});
+
+let renderer: THREE.WebGLRenderer | null = null;
+let scene: THREE.Scene | null = null;
+let camera: THREE.PerspectiveCamera | null = null;
+let cube: THREE.Mesh | null = null;
+
+const initThree = () => {
+  if (!contentRef.value) return;
+  // 初始化场景、相机和渲染器
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+
+  renderer = new THREE.WebGLRenderer({ canvas: contentRef.value });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // 创建一个立方体
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+};
+
+const animate = () => {
+  if (!renderer || !scene || !camera) return;
+  requestAnimationFrame(animate);
+  if (cube) {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  }
+  renderer.render(scene, camera);
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <canvas ref="content"></canvas>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
