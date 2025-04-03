e61acd36ff1e1b9677ce175848adbb1a69a7e0da
XieZongChen <952937570@qq.com>
2025-04-03 17:18:31 +0800

    feat: 引入 simplex-noise, 用噪声生成连续随机顶点

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { createNoise2D } from 'simplex-noise';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { onMounted, ref, useTemplateRef } from 'vue';
@@ -42,13 +43,20 @@ const createMesh = () => {
   if (!scene) return;
 
   // 创建一个 300 * 300 的平面几何体，宽高分成 10 段
-  const geometry = new THREE.PlaneGeometry(300, 300, 10, 10);
+  const geometry = new THREE.PlaneGeometry(300, 300, 100, 100);
+
+  // 山脉需要有连续性的随机，而不是这种完全随机，所以需要使用噪声函数
+  const noise2D = createNoise2D();
 
   const positions = geometry.attributes.position;
 
   for (let i = 0; i < positions.count; i++) {
-    // 随机顶点坐标
-    positions.setZ(i, Math.random() * 50);
+    // 传入 x、y 让噪音算法算出这个位置的 z
+    const x = positions.getX(i);
+    const y = positions.getY(i);
+    // 这个函数与 Math.random() 类似，返回 0 到 1 的数，只不过返回结果是与传入的 x、y 有关系的随机数，然后乘以 50 就是放大到 0 到 50
+    const z = noise2D(x / 100, y / 100) * 50;
+    positions.setZ(i, z);
   }
 
   const material = new THREE.MeshBasicMaterial({
